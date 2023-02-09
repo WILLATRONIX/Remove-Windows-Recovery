a4b9ad9a9f84871e540b20549678ba3eab41ec4d
WILLATRONIX <111201886+WILLATRONIX@users.noreply.github.com>
2023-02-09 01:02:47 +0000

    Add files via upload

---
+++ RemoveWindowsRecovery.ps1
@@ -0,0 +1,11 @@
+﻿#####################
+#                   #
+## Created by Will ##
+#                   #
+#####################
+
+#set to run at recovery
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\bootim.exe"
+
+#task kills itself
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\bootim.exe" -Name "Debugger" -Type "String" -Value "taskill /F /IM bootim.exe" -Force
